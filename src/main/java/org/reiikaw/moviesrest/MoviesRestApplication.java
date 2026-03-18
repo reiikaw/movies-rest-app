1db3d40c49dbf0a195ab3fad3a911a3d0d3760d8
Andrey Kamanin <akamanin@argo-studio.com>
2026-03-18 12:37:27 +0300

    Initial commit

---
+++ src/main/java/org/reiikaw/moviesrest/MoviesRestApplication.java
@@ -0,0 +1,13 @@
+package org.reiikaw.moviesrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/reiikaw/moviesrest/MoviesRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reiikaw.moviesrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
